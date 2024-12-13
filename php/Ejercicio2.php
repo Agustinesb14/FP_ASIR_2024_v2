ee861e7f3e60f5070b3a1401818ca68e6cc2ceb8
Agustin Escribano <Agustinescribano2010@gmail.com>
2024-12-13 08:46:02 +0100

    Añadir carpeta php con primer ejercicio

---
+++ php/Ejercicio2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1 - Introducción a PHP</title>
+</head>
+<body>
+    <h1>Ejercicio 1: ¿Que es PHP?</h1>
+    <h2>Preguntas y Respuestas:</h2>
+    <p>
+    <?php echo "1. ¿Dónde se ejecuta el código PHP: en el servidor o en el cliente?" ?><br>
+    <?php echo "El código PHP se ejecuta en el servidor." ?><br><br>
+    <?php echo "2. ¿Qué tipo de contenido puede generar PHP?" ?><br>
+    <?php echo "PHP puede generar contenido dinámico como páginas HTML, manejar cookies, procesar formularios, enviar correos electrónicos, y acceder a bases de datos." ?><br><br>
+    <?php echo "3. ¿Cuál es la diferencia entre PHP y JavaScript en términos de dónde se ejecutan?" ?><br>
+    <?php echo "PHP se ejecuta en el servidor antes de enviar el contenido al navegador, mientras que JavaScript se ejecuta en el cliente (en el navegador del usuario)." ?><br>
+    </p>
+</body>
+</html>
